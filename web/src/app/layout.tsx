4bdf61d91caf6c043b9616cf03971768e66ca46c
samirvalcacio <samir.valcacio@ufrr.br>
2025-09-29 09:27:51 -0400

    a

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -2,6 +2,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
+import { Analytics } from "@vercel/analytics/react"; 
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
